fe075d3635fe023d22400926da0c8e15dea69884
Aahlad Bysani <aahiaashu@gmail.com>
2026-06-30 11:46:59 -0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,11 @@ app = FastAPI(title="GroundTruth API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500"],
+    allow_origins=[
+        "http://localhost:5500",
+        "https://groundtruth.vercel.app",
+        "https://groundtruth-1.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
